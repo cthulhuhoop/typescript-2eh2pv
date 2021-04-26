0311b59dd3ca9b404e2bfe79406912ded6b33430
cthulhuhoop <adrian_k_jones@hotmail.com>
2021-04-26 22:12:41 +0100

    3 files modified

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # typescript-2eh2pv
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-2eh2pv)
+
+https://two.js.org/
+
--- index.ts
+++ index.ts
@@ -1,30 +1,36 @@
-// Import stylesheets
-import './style.css';
-import Two from 'two.js';
-// import {Two} from 'types.js';
+/// <reference path="two_types.d.ts" />
 
+// Import stylesheets
+import "./style.css";
+import Two from "two.js";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const appDiv: HTMLElement = document.getElementById("app");
+appDiv.innerHTML = `<h1>Two.js</h1>`;
 
 // Make an instance of two and place it on the page.
-var elem = document.getElementById('app');
-var params = { width: 285, height: 200 };
+var elem = document.getElementById("app");
+var params = { width: 360, height: 360 };
 var two = new Two(params).appendTo(elem);
 
 // two has convenience methods to create shapes.
-var circle = two.makeCircle(72, 100, 50);
+var circle = two.makeCircle(122, 50, 50);
 var rect = two.makeRectangle(213, 100, 100, 100);
- 
+var star = two.makeStar(133, 244, 152, 212, 18);
+
 // The object returned has many stylable properties:
-circle.fill = '#FF8660';
-circle.stroke = 'orangered'; // Accepts all valid css color
+circle.fill = "#FF8660";
+circle.stroke = "orangered"; // Accepts all valid css color
 circle.linewidth = 5;
 
-rect.fill = 'rgb(0, 200, 255)';
+rect.fill = "rgb(0, 200, 255)";
 rect.opacity = 0.55;
 rect.noStroke();
+
+// The object returned has many stylable properties:
+star.fill = "#F23e40";
+star.stroke = "yellow"; // Accepts all valid css color
+star.linewidth = 5;
 
 // Don't forget to tell two to render everything
 // to the screen
@@ -35,6 +41,6 @@ setInterval(() => {
   //rect.opacity = rect.opacity - 0.05;
 
   two.update();
-  }, 5)
+}, 5);
 
-var x : Two.Anchor = new Two.Anchor()
+var x: Two.Anchor = new Two.Anchor("sds");
